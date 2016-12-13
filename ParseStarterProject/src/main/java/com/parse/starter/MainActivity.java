2ece0502539b972d04cb6a04df9488ede157c9e2
okasha <okashoon@gmail.com>
2016-12-13 18:22:57 +0200

    first commit

---
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Switch;
+
+import com.parse.LogInCallback;
+import com.parse.ParseAnalytics;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Switch riderDriverSwitch;
+
+    public void getStarted(View view){
+
+
+    String riderOrDriver = "rider";
+        if(riderDriverSwitch.isChecked()){
+
+            riderOrDriver = "driver";
+
+        }
+        ParseUser.getCurrentUser().put("riderOrDriver", riderOrDriver);
+
+       redirect();
+
+
+    }
+
+    public void redirect(){
+        if(ParseUser.getCurrentUser().getString("riderOrDriver").equals("rider")){
+            Intent i = new Intent(getApplicationContext(),YourLocation.class);
+            startActivity(i);
+            Log.i("aaa","logged in as a rider");
+        }
+    }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ParseAnalytics.trackAppOpenedInBackground(getIntent());
+
+    getSupportActionBar().hide();
+
+    riderDriverSwitch = (Switch)findViewById(R.id.switch1);
+
+
+
+      if (ParseUser.getCurrentUser() == null) {
+          ParseAnonymousUtils.logIn(new LogInCallback() {
+              @Override
+              public void done(ParseUser user, ParseException e) {
+                  if(e == null){
+                      Log.i("aaa", "login successful");
+                  } else {
+                      Log.i("aaa", "login failed");
+                  }
+              }
+          });
+
+      }
+  }
+
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+}
---
+++ ParseStarterProject/src/main/java/com/parse/starter/YourLocation.java
@@ -0,0 +1,134 @@
+package com.parse.starter;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+public class YourLocation extends FragmentActivity implements OnMapReadyCallback,LocationListener {
+
+    private GoogleMap mMap;
+    LocationManager locationManager;
+    String provider;
+
+    Button requestUberButton;
+    TextView statusTextView;
+
+    boolean requested = false;
+
+    public void requestUber(View view){
+        if (requested == false) {
+            final ParseObject request = new ParseObject("Request");
+            request.put("rider", ParseUser.getCurrentUser().getUsername());
+            ParseACL acl = new ParseACL();
+            acl.setPublicReadAccess(true);
+            acl.setPublicWriteAccess(true);
+            request.setACL(acl);
+            request.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    requestUberButton.setText("Cancel UBER");
+                    statusTextView.setText("Ride requested");
+                    requested = true;
+
+
+                }
+            });
+        } else {
+
+
+
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_location);
+
+        requestUberButton = (Button)findViewById(R.id.requestButton);
+        statusTextView = (TextView) findViewById(R.id.statusTextView);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        locationManager = (LocationManager) getSystemService(getApplicationContext().LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(),false);
+        locationManager.requestLocationUpdates(provider,400,1,this);
+
+
+    }
+
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        if(location!=null){
+            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Your position"));
+
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        mMap.clear();
+        LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Your position"));
+
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
